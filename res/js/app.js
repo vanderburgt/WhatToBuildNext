ad77265b5b94791e1b367748e22f35cc94b91bfc
Eugene Skripnik <skripnike@gmail.com>
2018-03-31 02:19:04 +0300

    aside progress

--- res/js/app.js
+++ res/js/app.js
@@ -1,4 +1,14 @@
 'use strict';
 (function () {
-  // js
+  // sticky aside
+  window.onscroll = function() {myFunction()};
+  var navbar = document.getElementById('js-aside');
+  var sticky = 90; // var sticky = navbar.offsetTop;
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add('c-aside--sticky')
+    } else {
+      navbar.classList.remove('c-aside--sticky');
+    }
+  }
 })();
