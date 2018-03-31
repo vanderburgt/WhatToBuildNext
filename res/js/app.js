763800b580b800f885a77be410c85a8fee673488
Eugene Skripnik <skripnike@gmail.com>
2018-03-31 18:08:56 +0300

    responsive content block

--- res/js/app.js
+++ res/js/app.js
@@ -1,22 +1,33 @@
 'use strict';
 (function () {
-  // sticky aside
-  if ($('#js-aside').length) {
-    var aside = $("#js-aside");
-    $(document).scroll(function () {
-      var y = $(document).scrollTop();
-      if (y >= 110) {
-        aside.addClass('c-aside--sticky');
-      } else {
-        aside.removeClass('c-aside--sticky');
-      }
-    });
+  $(document).ready(function () {
+    // sticky aside
+    if ($('#js-aside').length) {
+      var aside = $("#js-aside");
+      $(document).scroll(function () {
+        var y = $(document).scrollTop();
+        if (y >= 110) {
+          aside.addClass('c-aside--sticky');
+        } else {
+          aside.removeClass('c-aside--sticky');
+        }
+      });
 
-    // reduce aside height if window scroll < 110px
-    $(document).ready(function () {
+      // reduce aside height if window scroll < 110px
       if ($(document).height() - $(window).height() < 110) {
         $('#js-aside__inner').addClass('c-aside__inner--short');
       }
-    });
-  }
-})();
+    }
+
+    // hide last coma in Content lsit
+    if ($('.c-content-list').length) {
+      $('.c-content-list').find('a').last().addClass('c-content-list__last-a');
+    }
+
+    if ($('.c-content-list__toggle')) {
+      $('.c-content-list__toggle').on('click', function () {
+        $(this).parent().toggleClass('c-content-list--collapsed');
+      });
+    }
+  });
+})(window, document);
